051a89514775cc4fa9705df8c7fde34200d62432
Janshu76 <janshumishra90@gmail.com>
2026-09-10 09:48:37 +0530

    final

--- app/config.py
+++ app/config.py
@@ -4,14 +4,15 @@ class Settings(BaseSettings):
     database_url: str
     jwt_secret:str
     jwt_algorithm:str
-    secret_key: str
+    secret_key:str
     access_token_expire_minutes: int = 30
     refresh_token_expire_days: int = 7
-    environment:str="devlopment"
+    environmenT:str="devlopment"
     fcm_credentials_path:str=""
     cors_origins:str="*"
     service_token: str
-
+    service_key: str
+    
     class Config:
         env_file = ".env"
 
